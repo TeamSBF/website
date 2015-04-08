153c7409950c0477c129a6baf2802d8bab1ae70d
csdemi <cdemianew@gmail.com>
2015-04-08 15:40:39 -0700

    Re-factored
    Re-factored the index page to now be a .php and made new file for
    header.php so that it takes out the header that every page needs

--- www/formtests/codys/Sit and Be Fit/index.php
+++ www/formtests/codys/Sit and Be Fit/index.php
@@ -1,27 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale = 1.0">
-  <title>Sit and Be Fit Research</title>
-  
-  <link rel="icon" href="favicon.ico">
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/grid.css">	
-  <link href='http://fonts.googleapis.com/css?family=Bitter|Bree+Serif' rel='stylesheet' type='text/css'>			
-  <link rel="stylesheet" href="css/styles.css">	
-	
-</head>
-<body>
-  <header class="grid_12 alpha">
-    <nav class="grid_12 alpha">
-      <ul>
-        <li><a href="index.html">Home</a></li>
-        <li><a href="about.html">About</a></li>
-        <li><a href="contact.html">Contact</a></li>
-      </ul>
-    </nav>
-  </header>
+<?php
+    require_once"header.php"; 
+?>
 	
   <div class="container">
     <div class="grid_8 alpha">
