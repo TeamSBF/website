4948959325fdcf5777b25d93f38fa93405ec6fa5
csdemi <cdemianew@gmail.com>
2015-04-08 16:14:12 -0700

    Refactoring
    Refactored the other two pages. Worked on color scheme and padding of
    lists

--- www/formtests/codys/Sit and Be Fit/header.php
+++ www/formtests/codys/Sit and Be Fit/header.php
@@ -16,9 +16,9 @@
   <header class="grid_12 alpha">
     <nav class="grid_12 alpha">
       <ul>
-        <li><a href="index.html">Home</a></li>
-        <li><a href="about.html">About</a></li>
-        <li><a href="contact.html">Contact</a></li>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
       </ul>
     </nav>
   </header>
