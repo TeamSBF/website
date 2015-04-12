b4d2632e4afce978a6805274fc32f90a79a66c7d
csdemi <cdemianew@gmail.com>
2015-04-12 14:14:59 -0700

    Updated
    Updated FAQ page to remove redundancy from the old websites information
    page

--- www/faq.php
+++ www/faq.php
@@ -2,19 +2,44 @@
     require_once"header.php"; 
 ?>
 <section>
-    <h1>FAQ</h1>  
+    <h1>FAQ</h1>
+    <h2>What is Sit and Be Fit?</h2>
+        <p>Sit and Be Fit believes that everyone has the right to feel good and age well. We do this by empowering people to improve their lives through safe, effective media-based health and wellness program. Sit and Be Fit is a non-profit organization committed to healthy aging advocacy.</p>
+        <p>For more information about Sit and Be Fit please visit the website at <a href="www.sitandbefit.org">www.sitandbefit.org</a></p>
+    <h2>What is the study about?</h2>
+        <p>About the study
+We are recruiting participants to watch Sit and Be Fit on public television, 3-5 times per week, for a 6-12 month period and monitor results via the internet using six functional assessments. You can begin the study at any time.</p>
+    
+    <h2>Who is conducting the study?</h2>
+        <p>Data from the study will be collected and analyzed by Sit and Be Fit.</p>
+    
+    <h2>What to expect if I participate?</h2>
+        <p>After enrolling in the project, you will be asked to conduct several standardized assessments to measure you functional fitness. Once baseline assessments have been logged, you will exercise 3-5 times weekly with Sit and Be Fit for a 6-12 month period. Every three months, self-assessments will be completed and logged again. At the completion of the study, you will fill out a brief exit survey and receive a summary analysis of your personal assessment results.</p>
+    
+    <h2>Why is this study important?</h2>
+        <p>To become an "evidence-based" exercise program for the older adults, research data must be collected, analyzed and published. In the fitness and public health industry, this qualitative validation is now necessary. As a participant, it will be valuable for you see how your commitment to regular exercise is impacting your quality of life.</p>
+    
     <h2>Will I need to participate for the entire 12 months?</h2>
         <p>Participating for 12 months is not a requirement. Every three months you will be asked to conduct assessments. Each round of assessments provides additional data that helps the overall research project.</p>
+    
     <h2>What if I am not able to exercise 3-5 times every week?</h2>
         <p>Ideally, you will be able to participate in Sit and Be Fit at least three times a week, however, we understand that there may be circumstances beyond your control that periodically prevent you from exercising that often.</p>
-    <h2>What if Sit and Be Fit airing times don't fit my schedule?</h2>
+    
+    <h2>What if Sit and Be Fit air times don't fit my schedule?</h2>
         <p>If you begin the study and your local television station changes the time that Sit and Be Fit is being aired (to a time that does not work for you), please call 888-678-9438 or (509)448-9438 to request a DVD so that you can continue exercising.</p>
+    
     <h2>Tell me more about the assessments</h2>
         <p><a href="assessments.php">click here</a> to learn more about the 6 self-assessments that you will be asked to conduct. We encourage you to use all 6 assessments at each 3-month testing interval, however, it is not mandatory to do so. In order to stay in the study group you must report results from at least one assessment at each of the testing intervals.</p>
+    
     <h2>Why are assessments important?</h2>
         <p>The functional fitness assessments used in this study are proven markers for determining whether or not you are at risk of losing your independence. They are good indicators of overall health and reveal areas that can be improved to ensure freedom through the aging process.</p>
+    
     <h2>Will my results and information be confidential?</h2>
         <p>All of your information will remain confidential and protected by HIPAA Click here for more information. rules All information that you share is confidential and protected under HIPAA rules. This means that your assessment results and personal information will remain anonymous. Once you complete the Enrollment Form Documents a number will become associated with your results, protecting your identity.</p>
+    
+    <h2>How to participate?</h2>
+        <p>Participation begins at when you enroll and continues for 12 months. You will need access to the Internet and access to the Sit and Be Fit television program (minimum of 3 times per week). Exercise programming from Sit and be Fit DVDs may be substituted for the broadcast version of the program.</p>
+    
     <h2>Who to contact with more questions</h2>
     <p>If you have additional questions, please contact the Sit and Be Fit offices toll free at 888-678-9438 or (509) 448-9438. You may also e-mail questions to <a href="mailto:sitandbefit@sitandbefit.org"> sitandbefit@sitandbefit.org </a></p>
 </section>
