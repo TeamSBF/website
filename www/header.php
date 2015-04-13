6b1d773088169be67141bf2ef83efd6d4fd3338b
csdemi <cdemianew@gmail.com>
2015-04-12 22:59:11 -0700

    Caught up on branch
    Realized branch was behind by 4 so needed to recommit it to fix all
    issues

--- www/header.php
+++ www/header.php
@@ -15,7 +15,6 @@
   <link rel="stylesheet" href="css/grid.css">	
   <link href='http://fonts.googleapis.com/css?family=Bitter|Bree+Serif' rel='stylesheet' type='text/css'>			
   <link rel="stylesheet" href="css/styles.css">	
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 	
 </head>
 <body>
@@ -25,7 +24,9 @@
         <li><a href="index.php">Home</a></li>
         <li><a href="about.php">About</a></li>
         <li><a href="contact.php">Contact</a></li>
-		<li><a href="register.php">Register</a></li>
+		<li><a href="faq.php">FAQ</a></li>
+        <li><a href="assessments.php">Assessments</a></li>
+        <li><a href="register.php">Register</a></li>
       </ul>
     </nav>
   </header>
