f9f434aab93b0095f967c943931bd85e27052d8a
Mason Varias <nosam276@gmail.com>
2015-05-09 18:22:06 -0700

    commented out unnecessary info for old users talbe

--- www/sql/old_users.php
+++ www/sql/old_users.php
@@ -3,10 +3,10 @@ $name = "old_users";
 $table = new CreateTable($name);
 $table->AddColumn('id')->SetAutoIncrement();
 $table->AddColumn('email')->MakeVarChar(100)->AddKey('unique');
-$table->AddColumn('password')->MakeVarChar(100);
-$table->AddColumn('pLevel')->MakeInt()->DefaultValue('1');
-$table->AddColumn('created')->MakeInt();
-$table->AddColumn('activated')->MakeBool()->DefaultValue('false');
+//$table->AddColumn('password')->MakeVarChar(100);
+//$table->AddColumn('pLevel')->MakeInt()->DefaultValue('1');
+//$table->AddColumn('created')->MakeInt();
+//$table->AddColumn('activated')->MakeBool()->DefaultValue('false');
 
 $population = array();
 return [$table, $population];
