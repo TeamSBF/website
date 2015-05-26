0cd00f869177982a2e12a8996db32f8b83af3c0d
sam25 <gosu747@hotmail.com>
2015-05-25 20:59:47 -0700

    added html to display checkboxes and date inputs for admincsv.php

--- www/admincsv.php
+++ www/admincsv.php
@@ -19,31 +19,48 @@ else if ($user && $user->AccessLevel >= 2)
 <div class="background">
 <h1>Data Retrieval [ADMIN]</h1>
 <p>You can download CSV (comma spererated values) files for various data here.</p>
+<div><label>You can choose to gather data within a time range by checking the box and picking dates</label></div>
 
 <div id="scriptMessage"></div>
 
-<h4>Assessments Data:</h4>
+<h3>Assessments Data:</h3>
 <button type="button" id="assessmentDl">Download</button>
+<div style="margin-top:15px;margin-bottom:15px"><input type="checkbox" id="assessmentBox"><label>Filter By Date</label>
+<label style="margin-left:50px;margin-right:10px">From:</label><input type="date" id="assesssmentDateStart">
+<label style="margin-left:20px;margin-right:10px">To:</label><input type="date" id="assesssmentDateEnd" value="<?php echo date('Y-m-d'); ?>"></div>
 
 <hr>
 
-<h4>Enrollment Form Data</h4>
+<h3>Enrollment Form Data</h3>
 <button type="button" id="enrollmentDl">Download</button>
+<div style="margin-top:15px;margin-bottom:15px"><input type="checkbox" id="enrollmentBox"><label>Filter By Date</label>
+<label style="margin-left:50px;margin-right:10px">From:</label><input type="date" id="enrollmentDateStart">
+<label style="margin-left:20px;margin-right:10px">To:</label><input type="date" id="enrollmentDateSEnd" value="<?php echo date('Y-m-d'); ?>"></div>
 
 <hr>
 
-<h4>Questionnaire Form Data</h4>
+<h3>Questionnaire Form Data</h3>
 <button type="button" id="questionnaireDl">Download</button>
+<div style="margin-top:15px;margin-bottom:15px"><input type="checkbox" id="questionnaireBox"><label>Filter By Date</label>
+<label style="margin-left:50px;margin-right:10px">From:</label><input type="date" id="questionnaireDateStart">
+<label style="margin-left:20px;margin-right:10px">To:</label><input type="date" id="questionnaireDateStart" value="<?php echo date('Y-m-d'); ?>"></div>
 
 <hr>
 
-<h4>ParQ Form Data</h4>
+<h3>ParQ Form Data</h3>
 <button type="button" id="parqDl">Download</button>
+<div style="margin-top:15px;margin-bottom:15px"><input type="checkbox" id="parqBox"><label>Filter By Date</label>
+<label style="margin-left:50px;margin-right:10px">From:</label><input type="date" id="parqDateStart">
+<label style="margin-left:20px;margin-right:10px">To:</label><input type="date" id="parqDateStart" value="<?php echo date('Y-m-d'); ?>"></div>
 </div>
 
 <script>
 
 /* event handlers to download the files */
+$(document).ready(function() { 
+	
+});
+
 $("#assessmentDl").on("click", function() {
 	window.location.href = 'admincsv.php?f=assessments';
 });
@@ -65,5 +82,3 @@ $("#parqDl").on("click", function() {
 <?php } ?>
 
 <?php require_once"footer.php"; ?>
-
-
