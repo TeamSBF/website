af741c56e6dcbed69e309d659ffe649e991a7ee9
Nyight <lightning_striker@hotmaill.com>
2015-05-27 13:16:49 -0700

    add pages to pages.php

--- www/scripts/Pages.php
+++ www/scripts/Pages.php
@@ -22,7 +22,9 @@ class Pages extends Singleton
                 "profile.php" => new Page("Profile", "profile.php", UserLevel::Member),
 				"cronSettings.php" => new Page("Settings", "cronSettings.php", UserLevel::Admin, "only"),
                 "logout.php" => new Page("Logout", "logout.php", UserLevel::Member),
-				"activation.php" => new Page("", "activation.php",UserLevel::Anon, "only", false)
+				"activation.php" => new Page("", "activation.php",UserLevel::Anon, "only", false),
+				"forgotPassword.php" => new Page("", "forgotPassword.php",UserLevel::Anon, "only", false),
+				"resetPassword.php" => new Page("", "resetPassword.php",UserLevel::Anon, "only", false)
             ];
             
             $this->items = $items;
